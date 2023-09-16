f61edaef9bb11c75a6be6907a5c41b5e14cda76c
prasun kumar <91335510+thunderxies@users.noreply.github.com>
2023-09-16 22:44:15 +0530

    had

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('button');
+const h1 = document.querySelector('h1');
+button.addEventListener('click', function() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    const newColor = `rgb(${r},${g},${b})`;
+    document.body.style.backgroundColor = newColor;
+    h1.innerText = newColor;
+})
